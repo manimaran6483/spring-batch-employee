97d7bb07bb51bd70785f25d3760f1a29239e53aa
901465 <901465@LTIN321899.cts.com>
2023-01-04 14:40:37 +0530

    update

--- src/main/java/com/batch/example/ExampleApplication.java
+++ src/main/java/com/batch/example/ExampleApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExampleApplication.class, args);
+		System.exit(SpringApplication.exit(SpringApplication.run(ExampleApplication.class, args)));;
 		//System.exit(0);
 	}
 }
